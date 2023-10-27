80cce0cfd1efb3fb3a688897cd517f7e2b1eb624
Dominik Gałenza <dominik.galenza@gmail.com>
2023-10-27 19:47:07 +0200

    Implemented movement by mouse.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -16,9 +16,9 @@ public class Unit : MonoBehaviour
 			transform.position += moveDirection * moveSpeed * Time.deltaTime;
 		}
 
-		if (Input.GetKeyDown(KeyCode.M))
+		if (Input.GetMouseButtonDown(0))
 		{
-			Move(new Vector3(4, 0, 4));
+			Move(MouseWorld.GetPosition());
 		}
 	}
 	private void Move(Vector3 targetPosition)
