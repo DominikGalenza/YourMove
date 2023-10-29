18bf703a1a36b8e2c557782bb1daff1c2e6a547b
Dominik Gałenza <dominik.galenza@gmail.com>
2023-10-29 21:26:27 +0100

    Added a selection visual indicator.

--- Assets/Scripts/UnitActionSystem.cs
+++ Assets/Scripts/UnitActionSystem.cs
@@ -5,8 +5,23 @@ using UnityEngine;
 
 public class UnitActionSystem : MonoBehaviour
 {
+	public static UnitActionSystem Instance { get; private set; }
+
+	public event EventHandler OnSelectedUnitChanged;
+
     [SerializeField] private Unit selectedUnit;
 	[SerializeField] private LayerMask unitLayerMask;
+
+	private void Awake()
+	{
+		if (Instance != null)
+		{
+			Debug.LogError($"There's more than one UnitActionSystem! {transform} {Instance}");
+			Destroy(gameObject);
+			return;
+		}
+		Instance = this;	
+	}
 
 	private void Update()
 	{
@@ -24,10 +39,21 @@ public class UnitActionSystem : MonoBehaviour
 		{
 			if (raycastHit.transform.TryGetComponent<Unit>(out Unit unit))
 			{
-				selectedUnit = unit;
+				SetSelectedUnit(unit);
 				return true;
 			}
 		}
 		return false;
+	}
+
+	private void SetSelectedUnit(Unit unit)
+	{
+		selectedUnit = unit;
+		OnSelectedUnitChanged?.Invoke(this, EventArgs.Empty);
+	}
+
+	public Unit GetSelectedUnit()
+	{
+		return selectedUnit;
 	}
 }
