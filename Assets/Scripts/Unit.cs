a162528fe5e6f9d69441acd7e720e15b4f5157d5
Dominik Gałenza <dominik.galenza@gmail.com>
2023-10-29 14:38:28 +0100

    Added two basic animations and transitions between them.

--- Assets/Scripts/Unit.cs
+++ Assets/Scripts/Unit.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class Unit : MonoBehaviour
 {
+	[SerializeField] private Animator unitAnimator;
+
 	private Vector3 targetPosition;
 	private float stoppingDistance = 0.1f;
 	private float moveSpeed = 4f;
@@ -14,6 +16,11 @@ public class Unit : MonoBehaviour
         {
 			Vector3 moveDirection = (targetPosition - transform.position).normalized;
 			transform.position += moveDirection * moveSpeed * Time.deltaTime;
+			unitAnimator.SetBool("IsWalking", true);
+		}
+		else
+		{
+			unitAnimator.SetBool("IsWalking", false);
 		}
 
 		if (Input.GetMouseButtonDown(0))
