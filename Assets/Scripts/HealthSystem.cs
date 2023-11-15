a0e8f63d0d4a416878228edeadacb94d896b704b
Dominik Gałenza <dominik.galenza@gmail.com>
2023-11-15 20:28:06 +0100

    Added a health bar and an additional action point visual counter.

--- Assets/Scripts/HealthSystem.cs
+++ Assets/Scripts/HealthSystem.cs
@@ -6,16 +6,26 @@ using UnityEngine;
 public class HealthSystem : MonoBehaviour
 {
     public event EventHandler OnDead;
+    public event EventHandler OnDamaged;
 
     [SerializeField] private int health = 100;
 
-    public void Damage(int damageAmount)
+    private int maxHealth;
+
+	private void Awake()
+	{
+		maxHealth = health;
+	}
+
+	public void Damage(int damageAmount)
     {
         health -= damageAmount;
         if (health < 0)
         {
             health = 0;
         }
+
+        OnDamaged?.Invoke(this, EventArgs.Empty);
 
         if (health == 0)
         {
@@ -27,4 +37,9 @@ public class HealthSystem : MonoBehaviour
 	{
 		OnDead?.Invoke(this, EventArgs.Empty);
 	}
+
+    public float GetHealthNormalized()
+    {
+        return (float)health / maxHealth;
+    }
 }
---
+++ Assets/Scripts/UI/UnitWorldUI.cs
@@ -0,0 +1,43 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UnitWorldUI : MonoBehaviour
+{
+    [SerializeField] private TextMeshProUGUI actionPointsText;
+    [SerializeField] private Unit unit;
+	[SerializeField] private Image healthBarImage;
+	[SerializeField] private HealthSystem healthSystem;
+
+	private void Start()
+	{
+		UpdateActionPointsText();
+		UpdateHealthBar();
+
+		Unit.OnAnyActionPointsChanged += Unit_OnAnyActionPointsChanged;
+		healthSystem.OnDamaged += HealthSystem_OnDamaged;
+	}
+
+	private void UpdateActionPointsText()
+    {
+        actionPointsText.text = unit.GetActionPoints().ToString();
+    }
+
+	private void UpdateHealthBar()
+	{
+		healthBarImage.fillAmount = healthSystem.GetHealthNormalized();
+	}
+
+	private void Unit_OnAnyActionPointsChanged(object sender, EventArgs e)
+	{
+		UpdateActionPointsText();
+	}
+
+	private void HealthSystem_OnDamaged(object sender, EventArgs e)
+	{
+		UpdateHealthBar();
+	}
+}
